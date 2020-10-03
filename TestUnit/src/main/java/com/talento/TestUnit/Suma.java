a9f64e98511ab90d10d87b29e64d72b9aad90828
crist <crist@LAPTOP-LRMTTMT7>
2020-10-02 21:13:43 -0300

    Primera subida en Github

---
+++ TestUnit/src/main/java/com/talento/TestUnit/Suma.java
@@ -0,0 +1,13 @@
+package com.talento.TestUnit;
+
+public class Suma {
+
+	public double sumar(double a, double b) {
+		return (a+b);
+	}
+	
+	public double duplicar(double a) {
+		return a+a;
+	}
+	
+}
---
+++ TestUnit/src/test/java/com/talento/TestUnit/TestResta.java
@@ -0,0 +1,13 @@
+package com.talento.TestUnit;
+
+import junit.framework.TestCase;
+
+public class TestResta extends TestCase{
+
+	public void testRestar() {
+		Resta resta = new Resta();
+		double resultado = resta.restar(2.0, 1.0);
+		//Verificando que la resta de los números es igual a 1.0
+		assertEquals(1.0, resultado);
+	}
+}
